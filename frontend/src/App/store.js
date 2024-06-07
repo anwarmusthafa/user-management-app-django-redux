cb008bad066c4a3d537337a42010f2ba9ca1895a
unknown <anwarmusthafa99@gmail.com>
2024-06-07 10:50:24 +0530

    final commit

--- frontend/src/App/store.js
+++ frontend/src/App/store.js
@@ -2,10 +2,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../redux/userSlice';
 
+
 const store = configureStore({
  reducer:{
    user: userReducer,
-
  }
 });
 
